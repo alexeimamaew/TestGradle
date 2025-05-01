43f4a2db2108c0838e4af28f096e22f4cd3d1347
Alexey <alexeimamaew@yandex.ru>
2025-05-01 16:19:14 +0300

    Initial commit

---
+++ src/test/java/SimpleJUnitTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleJUnitTests {
+    @Test
+    void calulateTest() {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        Assertions.assertEquals(4, sum);
+    }
+}
